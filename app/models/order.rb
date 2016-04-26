ab7b6250e34d17035b1f764d7759441b66752ba5
David Qorashi <david.qorashi@247-inc.com>
2016-04-26 13:51:35 -0700

    Fix some validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
 	validates :name, presence: true, length: { maximum: 30 }
 	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-	validates :phone, presence: true, length: { maximum: 15 }
-	validates :file_number, presence: true, uniqueness: true, numericality: true
+	validates :phone, length: { maximum: 15 }
+	validates :file_number, presence: true, uniqueness: true
 end
